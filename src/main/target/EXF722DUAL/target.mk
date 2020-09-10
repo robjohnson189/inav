3b6f30a506d43a5aeeb94d1c3e1129590c127695
Robert Johnson <robjohnson189@gmail.com>
2020-09-09 21:57:04 -0500

    starting target for EXF722DUAL

---
+++ src/main/target/EXF722DUAL/target.mk
@@ -0,0 +1,14 @@
+F7X2RE_TARGETS += $(TARGET)
+FEATURES    = VCP ONBOARDFLASH
+
+TARGET_SRC = \
+            drivers/compass/compass_fake.c \
+            drivers/accgyro/accgyro_spi_icm20689.c \
+            drivers/barometer/barometer_bmp280.c \
+						drivers/compass/compass_hmc5883l.c \
+            drivers/compass/compass_qmc5883l.c \
+            drivers/compass/compass_ist8310.c \
+            drivers/compass/compass_ist8308.c \
+            drivers/compass/compass_mag3110.c \
+            drivers/compass/compass_lis3mdl.c \
+            drivers/max7456.c
