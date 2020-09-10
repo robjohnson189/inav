c9e25b964a054f9f4053c3e0934a5340f9d673e1
Robert Johnson <robjohnson189@gmail.com>
2020-09-10 13:56:37 -0500

    try again

--- src/main/target/EXF722DUAL/target.mk
+++ src/main/target/EXF722DUAL/target.mk
@@ -2,7 +2,7 @@ F7X2RE_TARGETS += $(TARGET)
 FEATURES    = VCP ONBOARDFLASH
 
 TARGET_SRC = \
-            drivers/accgyro/accgyro_icm20689.c \
+            drivers/accgyro/accgyro_mpu6500.c \
             drivers/barometer/barometer_bmp280.c \
             drivers/compass/compass_ak8975.c \
             drivers/compass/compass_hmc5883l.c \
