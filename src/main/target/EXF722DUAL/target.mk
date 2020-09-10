5c83fb40bff4192dcaf6a7505153fb3500318c03
Robert Johnson <robjohnson189@gmail.com>
2020-09-09 22:21:00 -0500

    cleanup

--- src/main/target/EXF722DUAL/target.mk
+++ src/main/target/EXF722DUAL/target.mk
@@ -4,7 +4,7 @@ FEATURES    = VCP ONBOARDFLASH
 TARGET_SRC = \
             drivers/accgyro/accgyro_icm20689.c \
             drivers/barometer/barometer_bmp280.c \
-						drivers/compass/compass_hmc5883l.c \
+            drivers/compass/compass_hmc5883l.c \
             drivers/compass/compass_qmc5883l.c \
             drivers/compass/compass_ist8310.c \
             drivers/compass/compass_ist8308.c \
